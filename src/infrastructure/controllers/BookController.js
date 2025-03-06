842adb2b5194dda1818c13209af300c1eb983e07
Yordin Da Rocha <y.alayn@gmail.com>
2025-03-06 20:22:44 -0300

    Fix: Ajustes en controlador book

--- src/infrastructure/controllers/BookController.js
+++ src/infrastructure/controllers/BookController.js
@@ -3,8 +3,8 @@
 const express                = require('express');
 const router                 = express.Router();
 const BookRepositoryImpl     = require('../repositories/BookRepositoryImpl');
-const { registerBook }       = require('../../application/use_cases/registerBook');
-// const { findBookByBookname } = require('../../application/use_cases/findBook');
+const { registerBook }       = require('../../application/use_cases/book/registerBook');
+// const { findBookByBookname } = require('../../application/use_cases/book/findBook');
 const authMiddleware         = require('../middleware/auth');
 const { default: mongoose } = require('mongoose');
 
@@ -13,29 +13,11 @@ router.post('/register', authMiddleware, async (req, res) => {
   const bookRepository = new BookRepositoryImpl();
   const { title,_autor_id,_editorial_id,genre,publication_year,isbn } = req.body;
   try {
-
-    console.log("params: ",title,_autor_id,_editorial_id,genre,publication_year,isbn);
     const book = await registerBook(bookRepository, { title,_autor_id,_editorial_id,genre,publication_year,isbn });
     res.status(201).json(book);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
-
-// Ruta protegida para obtener usuario segun su bookname.
-// router.get('/', authMiddleware, async (req, res) => {
-//   const bookRepository = new BookRepositoryImpl();
-//   const { bookname } = req.body;
-//   try {
-//     const book = await findBookByBookname(bookRepository, { bookname });
-//     if (book) {
-//       res.status(200).json(book);
-//     } else {
-//       res.status(404).json({ error: `${bookname} Book not found` });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
 
 module.exports = router;
