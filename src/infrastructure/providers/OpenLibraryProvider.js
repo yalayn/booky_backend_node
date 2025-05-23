68b6ad32063258a3afae9ca7abb04a7391ad7605
Yordin Da Rocha <y.alayn@gmail.com>
2025-05-23 19:29:18 -0400

    Feat: Ajuste en libreria de api externa de libros

--- src/infrastructure/providers/OpenLibraryProvider.js
+++ src/infrastructure/providers/OpenLibraryProvider.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const BookSearchProvider = require('../../domain/providers/BookSearchProvider');
+const BookDataSearch     = require('../../domain/entities/BookDataSearch');
 
 class OpenLibraryProvider extends BookSearchProvider {
   async searchBooksByTitle(title) {
@@ -12,16 +13,33 @@ class OpenLibraryProvider extends BookSearchProvider {
       const randomPart = Math.floor(Math.random() * 1000000000).toString().padStart(9, '0');
       return `000-${randomPart}`;
     };
-    
-    return response.data.docs.slice(0, 10).map(book => ({
-        book_id  : book.key.split('/').pop(),
-        title    : book.title,
-        authors  : book.author_name?.join(', ') || 'Desconocido',
-        publisher: book.publisher?.[0] || 'Desconocida',
-        year     : book.first_publish_year || 'Desconocido',
-        isbn     : book.isbn?.[0] || generateUniqueISBN(),
-        cover_url: book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : null
+
+    const generateRandomKey = () => Math.random().toString(36).substr(2, 10).toUpperCase();
+
+    const books = response.data.docs.slice(0, 10).map(book => new BookDataSearch({
+      key               : book.key.split('/').pop(),
+      title             : book.title,
+      genre             : book.subject?.[0] || ['Desconocido'],
+      publication_year  : book.first_publish_year || 'Desconocido',
+      isbn              : book.isbn?.[0] || generateUniqueISBN(),
+      descriptions_short: 'No se encuentra la descripción.',
+      descriptions_long : 'No se encuentra la descripción.',
+      cover_i           : book.cover_i || null,
+      cover_url         : book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`: null,
+      editorial         : {
+        name         : book.publisher?.[0] || 'Desconocida',
+        country      : 'Desconocido',
+        founding_date: '1990-01-01',
+        key          : generateRandomKey()
+      },
+      author          : {
+        "name"    : book.author_name?.[0] || 'Desconocido',
+        "country" : 'Desconocido',
+        "birthday": '1990-01-01',
+        key       : book.author_key?.[0] || generateRandomKey(),
+      }
     }));
+    return books;
   }
 }
 
