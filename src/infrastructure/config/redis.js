c13f3c5524e713d338a50c77cf236463657c7c4a
Yordin Da Rocha <y.alayn@gmail.com>
2025-06-09 21:29:07 -0400

    Fix: Define url de redis

--- src/infrastructure/config/redis.js
+++ src/infrastructure/config/redis.js
@@ -1,7 +1,9 @@
 // src/infrastructure/config/redis.js
 
 const redis = require('redis');
-const client = redis.createClient();
+const client = redis.createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
 
 console.log('\nIniciando cliente redis...\n');
 
