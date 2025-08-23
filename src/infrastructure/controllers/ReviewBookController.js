1aca3eed1eb8b7ba9e0e1cccf97addb13d01ea93
Yordin Da Rocha <y.alayn@gmail.com>
2025-08-23 08:43:40 -0400

    Fix: Mejora en salida del api en reseña de libros

--- src/infrastructure/controllers/ReviewBookController.js
+++ src/infrastructure/controllers/ReviewBookController.js
@@ -8,32 +8,25 @@ const registerReviewUserBook   = require('../../application/use_cases/review_boo
 const findReviewUserBook       = require('../../application/use_cases/review_book/findReviewUserBook');
 
 router.post('/register', authMiddleware, async (req, res) => {
-    const userRepository       = new UserRepositoryImpl();
     const reviewBookRepository = new ReviewBookRepositoryImpl();
     const { book_id, review_text, rating } = req.body;
     try {
-        const user = await userRepository.findById(req.user.id);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
+        const userId = req.user._id;
+        const review = await registerReviewUserBook(reviewBookRepository, {userId, bookId: book_id, reviewText: review_text, rating});
+        if (!review) {
+            return res.status(404).json({ success:false, message: 'Review could not be registered' });
         }
-        const userId = user._id;
-        await registerReviewUserBook(reviewBookRepository, {userId, bookId: book_id, reviewText: review_text, rating});
-        res.status(201).json({ message: 'Review registered successfully' });
+        res.status(200).json({ success:true, message: 'Review registered successfully' });
     } catch (error) {
-      res.status(400).json({ error: error.message });
+        res.status(401).json({ success:false, message: error.message });
     }
 });
 
 router.get('/find', authMiddleware, async (req, res) => {
-    const userRepository       = new UserRepositoryImpl();
     const reviewBookRepository = new ReviewBookRepositoryImpl();
     const { book_id } = req.body;
     try {
-        const user = await userRepository.findById(req.user.id);
-        if (!user || !book_id) {
-            return res.status(404).json({ error: 'User not found or Book ID is required' });
-        }
-        const userId = user._id;
+        const userId = req.user._id;
         const reviews = await findReviewUserBook(reviewBookRepository,userId,book_id);
         res.status(200).json(reviews);
     } catch (error) {
